5bd21bd27e18323224fe3776d5c0a523481de193
Josep-Ratzinger <112721843+Josep-Ratzinger@users.noreply.github.com>
2024-05-19 16:58:51 -0500

    Add files via upload

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
-host_name = "your_host"
+host_name = "52.207.116.71"
 port_number = 3306
-user_name = "your_user"
-password_db = "your_password"
-database_name = "your_database"
+user_name = "root"
+password_db = "Ut3c120"
+database_name = ""
